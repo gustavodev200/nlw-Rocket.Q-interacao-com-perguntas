728a8f3f0177de85a084864ecfc7658ab2166841
Gustavo <gustavolage200@gmail.com>
2021-06-26 00:33:38 -0300

    Finalizando NLW - aprofundando no SQLite

---
+++ public/scripts/main.js
@@ -0,0 +1,46 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const modalTitle = document.querySelector(".modal h2")
+const modalDiscription= document.querySelector(".modal p")
+const modalButton = document.querySelector('.modal button')
+
+//pegar todos os botoes com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
+    button.addEventListener("click", handleClick)
+})
+ 
+//quando o botao delete for clicado ele abre a modal
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button =>{
+    button.addEventListener("click",(event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+
+    const text =  check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
+
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDiscription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    //abrir modal
+    modal.open();
+}
+
+
---
+++ public/scripts/modal.js
@@ -0,0 +1,22 @@
+export default function Modal(){
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener("click", close)
+
+    function open(){
+        //atribuir aa classe active para modal
+        modalWrapper.classList.add("active")
+    }
+    function close(){
+        //remover a classe active da modal
+        modalWrapper.classList.remove("active")
+    }
+
+
+    return {
+        open,
+        close
+    }
+}
